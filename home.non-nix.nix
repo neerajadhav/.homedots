26df1570599de25e80767652cef4735edb9385b9
neerajadhav <adhavneeraj9500@gmail.com>
2025-01-20 10:54:59 +0000

    new file:   home.non-nix.nix

---
+++ home.non-nix.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+let
+    userApps = with pkgs; [
+      motrix
+      gparted
+      brave
+      mendeley
+      vscodium-fhs
+      joplin-desktop
+      dropbox
+      conda
+      vlc
+      onlyoffice-desktopeditors
+      git
+    ];
+
+    appendApps = apps: userApps ++ apps; 
+in{
+    nixpkgs = {
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
+        };
+    };
+
+    home.username = "super";
+    home.homeDirectory = "/home/super";
+    home.packages = with pkgs; appendApps[];
+
+    # Do not change below configs
+    home.stateVersion = "24.11";
+    programs.home-manager.enable = true;
+}
